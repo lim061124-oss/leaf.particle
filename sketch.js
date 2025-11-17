550069ddaa3b15b2f1d9d28d8cb24fa71ced4f0b
lim061124-oss <lim061124@gmail.com>
2025-11-17 12:44:04 +0900

    class 분리 및 sketch 수정

--- sketch.js
+++ sketch.js
@@ -1,89 +1,28 @@
-class Particle {
-  constructor(position) {
-    this.acceleration = createVector(0, 0.05);
-    this.velocity = createVector(random(-1, 1), random(-1, 0));
-    this.position = position.copy();
-    this.lifespan = 255;
-  }
-
-  applyForce(force) {
-    this.acceleration.add(force);
-  }
-
-  update() {
-    this.velocity.add(this.acceleration);
-    this.position.add(this.velocity);
-    this.lifespan -= 2;
-  }
-
-  display() {
-    stroke(200, this.lifespan);
-    strokeWeight(2);
-    fill(127, this.lifespan);
-    ellipse(this.position.x, this.position.y, 12, 12);
-  }
-
-  isDead() {
-    return this.lifespan < 0;
-  }
-}
-
-class ParticleSystem {
-  constructor(origin) {
-    this.origin = origin.copy();
-    this.particles = [];
-    this.forces = []; 
-  }
-
-  addParticle() {
-    this.particles.push(new Particle(this.origin));
-  }
-
-  addForce(force) {
-    this.forces.push(force.copy());
-  }
-
-  run() {
-    for (let i = this.particles.length - 1; i >= 0; i--) {
-      let p = this.particles[i];
-
-      for (let f of this.forces) {
-        p.applyForce(f);
-      }
-
-      p.update();
-      p.display();
-
-      if (p.isDead()) {
-        this.particles.splice(i, 1);
-      }
-    }
-
-    this.forces = [];
-  }
-}
-
-let system;
+let systems = [];
 
 function setup() {
   createCanvas(720, 400);
-  system = new ParticleSystem(createVector(width / 2, 50));
+  for (let i=0;i<3;i++){
+    systems.push(new LeafSystem(createVector(100+i*200, 50)));
+  }
 }
 
 function draw() {
-  background(51);
+  background(200, 230, 255);
+  let gravity = createVector(0,0.05);
+  let wind = createVector(random(-0.02,0.02),0);
 
-  let gravity = createVector(0, 0.05);
-  let wind = createVector(random(-0.02, 0.02), 0);
-
-  system.addForce(gravity);
-  system.addForce(wind);
-
-  system.addParticle();
-  system.run();
+  for (let sys of systems){
+    sys.addLeaf();
+    sys.addForce(gravity);
+    sys.addForce(wind);
+    sys.run();
+  }
 }
 
 function mousePressed() {
-  let burst = createVector(0, -0.5);
-  system.addForce(burst);
+  let burst = createVector(0,-0.5);
+  for (let sys of systems){
+    sys.addForce(burst);
+  }
 }
